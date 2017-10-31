a331e1279784411c5d0f6c6a36d2806208ea5196
김광철 <anyman@afreecatv.com>
2017-10-31 13:55:14 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NumberComponent } from './number/number.component';
+import { ClockComponent } from './clock/clock.component';
+import { DotComponent } from './dot/dot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberComponent,
+    ClockComponent,
+    DotComponent
   ],
   imports: [
     BrowserModule
